0a8777093f4e69a1b28254029f583211db6c3e05
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-11 16:24:50 +0200

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,16 +1,11 @@
-function weryfikuj()
-{
+function weryfikuj() {
+  var password = document.getElementById("password").value;
+  var d = password.length;
 
-var password = document.getElementById("password").value;
-var d = password.length;
-
-if (password == "")
-{
-document.getElementById("result").innerHTML = '<span style="color: red;">Enter password.</span>';
-}
-else if (d <=7)
-{
-document.getElementById("result").innerHTML = '<span style="color: red;">Password is too weak.</span>';
-}
-
+  if (password == "") {
+    document.getElementById("result").innerHTML = '<span style="color: red;">Enter password.</span>';
+  }
+  else if (d <=7) {
+    document.getElementById("result").innerHTML = '<span style="color: red;">Password is too weak.</span>';
+  }
 }
