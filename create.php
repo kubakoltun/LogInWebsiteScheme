f10692172827d01c0166731173ca9b5caca850e7
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-13 13:36:02 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,12 +1,8 @@
 <?php 
-
 $conn = mysqli_connect("localhost", "root", "", "db") or die("Database connection error!");
-	
 		$login = $_POST["login"];
 		$haslo = $_POST["password"];
 		$q = "CREATE USER '$login'@'localhost' IDENTIFIED BY '$password'";
 		$result = mysqli_query($conn, $q) or die ("User creation failed.");
-		
 	mysqli_close($conn);
-	
 ?>
